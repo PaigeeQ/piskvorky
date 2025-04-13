b550a625e69ac6f1727da408ce0900b39304ea3b
PaigeeQ <pa.krajcova@gmail.com>
2025-04-13 21:23:19 +0200

    predelan komplest hra.js

--- hra.js
+++ hra.js
@@ -1,33 +1,32 @@
 let currentPlayer = "circle";
 
-const policka = document.querySelectorAll(".čtverec");
-
-policka.forEach((policko) => {
-    policko.addEventListener("click", () => {
-        if (policko.classList.contains("čtverec--circle"))
-           (policko.classList.contains("čtverec--cross"))
-         { return 
-        }
-        if (currentPlayer === "circle") {
-            policko.classList.add("čtverec--circle")
-            currentPlayer = "cross"
-        } else {
-            policko.classList.add("čtverec--cross")
-            currentPlayer = "circle"
-        }
-    })
-});
-
 const handleClick = (event) => {
     const clickCtverce = event.target
+
     if (clickCtverce.classList.contains("čtverec--circle") ||
        clickCtverce.classList.contains("čtverec--cross"))
+
     { return 
-    }
+
+    } if (currentPlayer === "circle") {
         clickCtverce.classList.add("čtverec--circle")
+        currentPlayer = "cross"
+
+    } else {
+        clickCtverce.classList.add("čtverec--cross")
+        currentPlayer = "circle"
+    };
+
+const ikona = document.querySelector(".icon4");
+    if (currentPlayer === "circle") {
+        src = ikona.src = "obrazky/circle.svg";
+    } else {
+        src = ikona.src = "obrazky/cross.svg";
+}
 };
 
 const hraciPole = document.querySelectorAll(".čtverec");
-hraciPole.forEach((pole) => {
-    pole.addEventListener("click", handleClick)
-});
+
+hraciPole.forEach((policko) => {
+    policko.addEventListener("click", handleClick)
+   });
